7300ffbd28eb59f3e5d4edcdb301b81927085d39
Yuval Maron <yuvalmaron222@gmail.com>
2024-04-03 10:38:45 +0300

    Initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Date Pickers',
+  description: "The most unhinged date-pickers you've ever seen!",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
